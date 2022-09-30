883e2b2c9a151b76d16ec9661b40cb3884e5b980
Manesha  P <114483149+manesha02@users.noreply.github.com>
2022-09-30 12:40:34 +0000

    solved problems

--- set01/problem08.c
+++ set01/problem08.c
@@ -7,10 +7,10 @@ void output(int n, int a[n],int sum);
 int main(){
   int n,sum;
   n = input_array_size();
-  int a[n]={};
-  input_array(n,a[n]);
-  sum =sum_n_array(n,a[n]);
-  output(n,a[n],sum);
+  int a[n];
+  input_array(n,a);
+  sum =sum_n_array(n,a);
+  output(n,a,sum);
 }
 
 int input_array_size(){
@@ -23,7 +23,7 @@ void input_array(int n, int a[n]){
   int i;
   for(i = 0;i<n;i++){
     printf("Enter a number\n");
-    scanf("%d", a[i]);
+    scanf("%d", &a[i]);
   }
 }
 
@@ -39,7 +39,7 @@ void output(int n, int a[n],int sum){
   printf("%d",a[0]);
   int i;
   for(i = 1;i<n;i++){
-    printf("+ %d", a[i]);
+    printf("+%d", a[i]);
   }
-  printf("is %d",sum);
+  printf(" is %d",sum);
 }
