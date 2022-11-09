3ae06c66a81b9e5a08cf1f2d93c2637a95f8ac20
Manesha  P <114483149+manesha02@users.noreply.github.com>
2022-11-09 07:32:13 +0000

    solved problems

--- set02/problem05.c
+++ set02/problem05.c
@@ -18,20 +18,22 @@ int input(){
   return(a);
 }
 
+//1 + (x^1)/3! + (x^2)/5! + (x^3)/7! + ...
 float borga_X(int x){
-  float sum=0.0,fact = 1;;
-  int i = 1,i1 = 3,c = 1,a = 1;
+  float sum=0.0,fact = 1.0,c = 1.0;
+  int i = 1,i1 = 3,a;
   while(c>0.000001){
     for(a = 1;a<=i1;a++){
       fact = fact*a;
     }
     sum = sum+c;
-    c = pow(x,i)/fact;
+    c = (pow(x,i))/fact;
+    printf("%f\n", c);
     i++;
     i1 += 2;
     fact = 1;
   }
-  return(c);
+  return(sum);
 }
 
 void output(int x, float result){
